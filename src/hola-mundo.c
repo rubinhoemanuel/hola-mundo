d7580218a74f09006c1fa478c5b77eb24f0bf15f
Emanuel Rubinho <rubinhoemanuel@gmail.com>
2022-04-15 00:04:35 -0300

    Se sube ejercicio hola-mundo C y se modifica archivo .gitignore

---
+++ src/hola-mundo.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : hola-mundo.c
+ Author      : Emanuel Rubiño
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
